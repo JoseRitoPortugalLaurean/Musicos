949ccc49cfb28e0e70c11db75f3072d197356407
José Rito Portugal Laureán <126290786+JoseRitoPortugalLaurean@users.noreply.github.com>
2023-03-23 19:56:23 +0000

    updated abstracts

--- musicos/Program.cs
+++ musicos/Program.cs
@@ -1,6 +1,8 @@
 ﻿abstract class Musico //clase abstracta que solo puede generar subclases, pero no objetos diretamente
 {
     public string Nombre { get; set; }
+    public virtual string GetSaludo() {return $"Hola, soy {Nombre}";}//método virtual que puede ser sobreescrito por las subclases
+    public virtual void Saludo() {Console.WriteLine(GetSaludo());}//método virtual que puede ser sobreescrito por las subclases
     public Musico(string n) {Nombre = n;}
     public void Saluda() {Console.WriteLine($"Hola, soy {Nombre}");}
     public abstract void Toca();//para implementar métodos abstractos, debemos de usar la palabra reservada override
@@ -17,6 +19,8 @@
         public string? Batería { get; set; }
         public Baterista (string n, string b) : base(n) {Batería = b;}
         public override void Toca() {Console.WriteLine($"{Nombre} está tocando la batería");}
+        public override string GetSaludo() => base.GetSaludo() + " y lamentablemente soy baterista";
+        public override void Saludo() {Console.WriteLine(GetSaludo());}
     }
 
 internal class Program
